4896ddb68584ccade8d3f34c2a771d24c265e6fc
Ondrej Sika <ondrej@ondrejsika.com>
2017-01-08 23:14:46 +0100

    use slushpool

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 # Example usage. You can try it for a hour as a donation :-)
 #./epycyzm.py stratum+tcp://1Fqxr6JAMTtqfQPHSZajiEPwnT4b6fXpaH.worker1:z@equihash.eu.nicehash.com:3357
-nice -n9 ./epycyzm.py stratum+tcp://t1cPGD3u1ooL4s9mrKGnbRAu6hH6tntdCy3:x@zmine.io:1337
+nice -n9 ./epycyzm.py stratum+tcp://ondrejsika:x@eu.zec.slushpool.com:4444
 
